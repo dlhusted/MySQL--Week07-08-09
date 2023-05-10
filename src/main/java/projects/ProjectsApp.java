862dee26ee954d7c90f52ea40a0dc4432132cf0e
Dana Husted <122566507+dlhusted@users.noreply.github.com>
2023-05-10 10:27:39 -0700

    week 10 update

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -11,11 +11,15 @@ import projects.service.ProjectService;
 
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
-	
+	private Project curProject;
 
 	// @ formatter:off
 	private List<String> operations = List.of(
-			"1) Add a project");
+			"1) Add a project",
+			"2) List projects",
+			"3) Select a project."
+			);
+	
 	// @ formatter:on
 
 	private ProjectService projectService = new ProjectService();
@@ -50,6 +54,13 @@ public class ProjectsApp {
 				case 1:
 					createProject();
 					break;
+				case 2:
+					listProjects();
+					break;
+				case 3:
+					selectProject();
+					break;
+					
 				default: System.out.println("\n" + selection + " is not a valid selection. Try again.");
 					break;
 				}
@@ -58,6 +69,29 @@ public class ProjectsApp {
 			}
 		}
 	}
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		curProject = null;
+		curProject = projectService.fetchProjectById(projectId);
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nInvalid project ID selected.");
+		} 
+	}
+
+
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out
+				.println("    " + project.getProjectId()
+					+ ": " + project.getProjectName()));
+		
+	}
+
+
 /*
  * see recipes video for method: addRecipe()
  */
@@ -114,6 +148,15 @@ public class ProjectsApp {
 		System.out.println();
 		System.out.println("\nThese are the available selections: Press Enter key to quit: ");
 		operations.forEach(line -> System.out.println("   " + line));
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		}
+		else {
+			System.out.println("\nYou are working with project: " + curProject);
+		}
+		
+		
 	}
 	
 	private Integer getIntInput(String prompt) {
